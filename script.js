40f1111df69b0ca0bdff4555ef71ab329ae482f6
Diego Quintela <diegolucasq@gmail.com>
2020-12-14 14:51:48 -0500

    Add files via upload
    Bootstrap Modal implemented on Join Button.
    Mailchimp integration.
    Footer fixed

--- script.js
+++ script.js
@@ -5,29 +5,39 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsShowModal = document.querySelectorAll(".show-modal");
 
-// we can remove or add classes to the html
-const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
+// Modal
 
-const openModal = function () {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-};
+const callSubmit = () => document.forms[0].submit;
+
+// var myModal = document.getElementById("myModal");
+// var myInput = document.getElementById("myInput");
+
+// myModal.addEventListener("shown.bs.modal", function () {
+//   myInput.focus();
+// });
+// we can remove or add classes to the html
+// const closeModal = function () {
+//   modal.classList.add("hidden");
+//   overlay.classList.add("hidden");
+// };
+
+// const openModal = function () {
+//   modal.classList.remove("hidden");
+//   overlay.classList.remove("hidden");
+// };
 
 console.log(btnsShowModal);
 
-for (let i = 0; i < btnsShowModal.length; i++)
-  btnsShowModal[i].addEventListener("click", openModal);
+// for (let i = 0; i < btnsShowModal.length; i++)
+//   btnsShowModal[i].addEventListener("click", openModal);
 
-btnCloseModal.addEventListener("click", closeModal);
+// btnCloseModal.addEventListener("click", closeModal);
 // we are not calling the function because we want it to be executed only after the click event happens.
 //if we had called the function, it would've execute at first
-overlay.addEventListener("click", closeModal);
+// overlay.addEventListener("click", closeModal);
 
 //responding to keyboard(global) events
-// Here we are just 'listening' for an envent, now we need to look for an event
+// Here we are just 'listening' for an e vent, now we need to look for an event
 document.addEventListener("keydown", function (e) {
   if (e.key === "Escape") {
     if (!modal.classList.contains("hidden")) {
